820949728aa05570a1163b7de3ea23d8b5381362
silvershakes <pravinbalgore2850@gmail.com>
2022-04-11 09:50:52 +0530

    4thcommit

---
+++ src/pattern/A11april1.java
@@ -0,0 +1,39 @@
+package pattern;
+
+public class A11april1 {
+
+	public static void main(String[] args) 
+	{
+		// **********
+		
+		
+		//10 stars
+		
+		for(int i=1;i<=20;i++)
+		{
+			System.out.print("*");
+		}
+		
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+		//*
+	
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("*");
+		}
+		
+
+	}
+
+}
---
+++ src/pattern/C11thapril.java
@@ -0,0 +1,29 @@
+package pattern;
+
+public class C11thapril {
+
+	public static void main(String[] args)
+    {
+//		*****
+//		****
+//		***
+//		**
+//		*
+		
+		int star=5;
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=star;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			star--;
+		}
+		
+		
+
+	}
+
+}
